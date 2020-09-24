613b94f40f90d4ae9c20b7c5fb5cf6d4e4414904
R.S <dgechain@yahoo.co.jp>
2020-09-24 15:50:36 +0900

    Added Progress class

---
+++ product/app/models/Progress.scala
@@ -0,0 +1,27 @@
+package models
+
+import anorm.{ ~, RowParser}
+import anorm.SqlParser._
+
+import play.api.libs.json.{Format, Json}
+
+object Progress {
+  implicit val format: Format[Progress] = Json.format[Progress]
+}
+
+object SimpleProgress {
+  implicit val format: Format[SimpleProgress] = Json.format[SimpleProgress]
+  implicit val simpleProgressParser: RowParser[SimpleProgress] =
+    get[Option[Long]]("progress.id") ~
+      get[String]("progress.name") map {
+      case id ~ name => SimpleProgress(id, name)
+    }
+
+}
+
+case class Progress(id: Option[Long]=None,
+                   orderNumber: Int,
+                   name: String) {}
+
+case class SimpleProgress(id: Option[Long]=None,
+                         name: String)
