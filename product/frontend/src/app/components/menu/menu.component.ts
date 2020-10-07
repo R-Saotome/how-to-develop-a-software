ddee50d642cede387eb1c4c504fdc68a96b7bd62
R-Saotome <saotome@dev-mgr.com>
2020-10-07 15:06:13 +0900

    Added styles with matRippleModules

--- product/frontend/src/app/components/menu/menu.component.ts
+++ product/frontend/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,16 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-  menus: { icon: string; name: string }[] = [
-    { icon: 'calendar_today', name: 'スケジュール' },
-    { icon: 'business', name: '顧客情報' },
-    { icon: 'contacts', name: '名刺情報' },
-    { icon: 'folder_open', name: '案件情報' },
-    { icon: 'library_books', name: '日報' },
-    { icon: 'build', name: '設定' },
+  menus: { icon: string; name: string; href: string }[] = [
+    { icon: 'calendar_today', name: 'スケジュール', href: '/schedules' },
+    { icon: 'business', name: '顧客情報', href: '/companies' },
+    { icon: 'contacts', name: '名刺情報', href: '/persons' },
+    { icon: 'folder_open', name: '案件情報', href: '/opportunities' },
+    { icon: 'library_books', name: '日報', href: '/reports' },
+    { icon: 'build', name: '設定', href: '/settings' },
   ];
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onNavigateTo(url: string) {
+    this.router.navigateByUrl(url);
+  }
 }
--- product/frontend/src/app/components/menu/menu.module.ts
+++ product/frontend/src/app/components/menu/menu.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, MatListModule, MatIconModule, MatRippleModule],
   exports: [MenuComponent],
   providers: [],
 })
