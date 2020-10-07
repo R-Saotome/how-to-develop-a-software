700c2613b64b21fa7766dc7b2d0a680963af9b00
R-Saotome <saotome@dev-mgr.com>
2020-10-07 17:21:26 +0900

    Added delete function

--- product/frontend/src/app/pages/company/company-detail/company-detail.component.ts
+++ product/frontend/src/app/pages/company/company-detail/company-detail.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { DialogComponent } from 'src/app/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-company-detail',
   templateUrl: './company-detail.component.html',
-  styleUrls: ['./company-detail.component.scss']
+  styleUrls: ['./company-detail.component.scss'],
 })
 export class CompanyDetailComponent implements OnInit {
   isEditMode = false;
+  constructor(public dialog: MatDialog, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onDelete() {
+    const dialogRef = this.dialog.open(DialogComponent);
+    dialogRef.afterClosed().subscribe((isAccepted) => {
+      if (isAccepted) {
+        // TODO delete data by ID
+
+        this.router.navigateByUrl('/companies');
+      }
+    });
   }
-
 }
