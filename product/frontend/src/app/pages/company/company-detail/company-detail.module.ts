61397350f1395d5dbfcba76e54579cfb73d1541c
R-Saotome <saotome@dev-mgr.com>
2020-10-07 17:30:21 +0900

    Added company form page

--- product/frontend/src/app/pages/company/company-detail/company-detail.module.ts
+++ product/frontend/src/app/pages/company/company-detail/company-detail.module.ts
@@ -6,9 +6,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [CompanyDetailComponent],
+  declarations: [CompanyDetailComponent, FormComponent],
   imports: [
     CommonModule,
     MatCardModule,
