fff79fde6d8ac8a95bddd8406a98e011f794dafa
R-Saotome <saotome@dev-mgr.com>
2020-10-07 10:49:04 +0900

    Added styles

--- product/frontend/src/app/pages/company/company-detail/company-detail.module.ts
+++ product/frontend/src/app/pages/company/company-detail/company-detail.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyDetailComponent } from './company-detail.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CompanyDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatTabsModule, MatButtonModule],
   exports: [CompanyDetailComponent],
 })
 export class CompanyDetailModule {}
