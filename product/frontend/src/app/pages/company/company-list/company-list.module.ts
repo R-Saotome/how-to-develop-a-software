0a395ae6f521406597cf69b0b723dc0fde8b4f51
R-Saotome <saotome@dev-mgr.com>
2020-10-08 17:18:01 +0900

    Added fab

--- product/frontend/src/app/pages/company/company-list/company-list.module.ts
+++ product/frontend/src/app/pages/company/company-list/company-list.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyListComponent } from './company-list.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { TableModule } from 'src/app/components/table/table.module';
 
 @NgModule({
   declarations: [CompanyListComponent],
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, MatButtonModule, MatIconModule],
   exports: [CompanyListComponent],
 })
 export class CompanyListModule {}
