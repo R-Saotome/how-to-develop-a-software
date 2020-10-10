905041fcfb0a3624fd6d88e46c0741cbae22b694
R-Saotome <saotome@dev-mgr.com>
2020-10-10 17:18:36 +0900

    Fixed styles

--- product/frontend/src/app/components/calendar/calendar.component.ts
+++ product/frontend/src/app/components/calendar/calendar.component.ts
@@ -8,15 +8,16 @@ import { CalendarOptions } from '@fullcalendar/angular';
 })
 export class CalendarComponent implements OnInit {
   constructor() {}
-  ngOnInit(): void {}
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this),
+    height: '80vh',
     events: [
       { title: 'event 1', date: '2019-04-01' },
       { title: 'event 2', date: '2019-04-02' },
     ],
   };
+  ngOnInit(): void {}
 
   handleDateClick(arg) {
     alert('date click! ' + arg.dateStr);
