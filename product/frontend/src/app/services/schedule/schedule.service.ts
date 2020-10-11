fa8eed7fc8475e646fba3cb8e066ce8476b338a0
R-Saotome <saotome@dev-mgr.com>
2020-10-11 18:43:02 +0900

    Added schedule service

---
+++ product/frontend/src/app/services/schedule/schedule.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Schedule } from 'src/app/interface/schedule.interface';
+import { environment } from 'src/environments/environment';
+
+const SCHEDULE_SUFFIX = 'schedules';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScheduleService {
+  constructor(private http: HttpClient) {}
+
+  scheduleList$: Observable<Schedule[]>;
+
+  fetch(): void {
+    this.scheduleList$ = this.http
+      .get(`${environment.BASE_API_URL}/${SCHEDULE_SUFFIX}`)
+      .pipe(map((d: any) => d.data));
+  }
+
+  getAll(): Observable<Schedule[]> {
+    return this.scheduleList$;
+  }
+
+  getById(id: any) {}
+
+  add(company: Schedule) {
+    return this.http.post(
+      `${environment.BASE_API_URL}/${SCHEDULE_SUFFIX}`,
+      company
+    );
+  }
+}
