06336e385c27097e2848e5144f39a0ce1dbddb62
R-Saotome <saotome@dev-mgr.com>
2020-10-11 12:44:45 +0900

    Added events

--- product/frontend/src/app/components/calendar/calendar.component.ts
+++ product/frontend/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
@@ -7,19 +7,35 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
-  constructor() {}
+  @Output() eventClicked = new EventEmitter();
+  @Output() dateClicked = new EventEmitter();
+
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this),
     height: '80vh',
+    dateClick: this.handleDateClick.bind(this),
+    eventClick: this.handleEventClick.bind(this),
     events: [
-      { title: 'event 1', date: '2019-04-01' },
+      {
+        title: 'event 1',
+        start: '2020-09-29T00:00:00.000+09:00',
+        end: '2020-09-29T12:00:00.000+09:00',
+        allDay: true,
+      },
       { title: 'event 2', date: '2019-04-02' },
     ],
   };
+
+  constructor() {}
   ngOnInit(): void {}
 
   handleDateClick(arg) {
     alert('date click! ' + arg.dateStr);
+    this.dateClicked.emit(arg.dateStr);
+  }
+
+  handleEventClick(arg) {
+    alert('date click! ' + arg.event.id);
+    this.eventClicked.emit(arg.event.id);
   }
 }
