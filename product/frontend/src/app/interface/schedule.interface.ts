e6ede599fccf0d3951893349ba5dd26255388388
R-Saotome <saotome@dev-mgr.com>
2020-10-13 13:39:43 +0900

    Added properties

--- product/frontend/src/app/interface/schedule.interface.ts
+++ product/frontend/src/app/interface/schedule.interface.ts
@@ -5,6 +5,19 @@ export interface Schedule {
   end_date: Date;
   title: string;
   note?: string;
+  company?: {
+    id: any;
+    name: string;
+  };
+  person?: {
+    id: any;
+    first_name: string;
+    last_name: string;
+  };
+  opportunity?: {
+    id: any;
+    name: string;
+  };
   members: {
     id: any;
     first_name: string;
